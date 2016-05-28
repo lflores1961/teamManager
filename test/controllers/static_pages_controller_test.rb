08d28293358a4e8e02185e6e6bf661d17b830ab9
Fernando Flores <fernando.flores1961@gmail.com>
2016-05-28 15:32:53 -0500

    Set up main menu initial frame with bootstrap

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,12 +4,16 @@ class StaticPagesControllerTest < ActionController::TestCase
 
   def setup
     @base_title = "TEAM MANAGER"
+    @opciones = %w{Registro_de_Jugadores Registro_de_Pagos Mantenimiento Consulta_Jugador Reporte_Global_PDF}
   end
 
   test "should get main_menu" do
     get :main_menu
     assert_response :success
     assert_select "title", "Inicio | #{@base_title}"
+    @opciones.each do |op1|
+      assert_select "a", op1
+    end
   end
 
 end
