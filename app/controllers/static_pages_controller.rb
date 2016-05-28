40a3d1b92dc110660a546558a4577a7ff602caa8
Fernando Flores <fernando.flores1961@gmail.com>
2016-05-28 02:20:26 -0500

    fix static pages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
+
   def main_menu
     @options = %w(Registro_de_Jugadores Registro_de_Pagos Mantenimiento
     Consulta_Jugador Reporte_Global_PDF) 
